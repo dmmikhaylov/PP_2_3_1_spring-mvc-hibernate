c3e22bcaad348fe2406bd873fc85d9740fbb2ded
dmmikhaylov <dmmikhaylov.job@gmail.com>
2023-01-15 15:42:25 +0300

    2_3_1

---
+++ src/main/java/web/dao/UserDAOImpl.java
@@ -0,0 +1,43 @@
+package web.dao;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import web.model.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class UserDAOImpl implements UserDAO{
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public List<User> getAllUsers() {
+        return entityManager.createQuery("SELECT u FROM User u", User.class).getResultList();
+    }
+
+    @Override
+    public void save(User user) {
+        entityManager.persist(user);
+    }
+
+    @Override
+    public User getUserById(int id) {
+        return entityManager.find(User.class, id);
+    }
+
+    @Override
+    public void update(User updatedUser) {
+        entityManager.merge(updatedUser);
+    }
+
+    @Override
+    public void delete(int id) {
+        entityManager.remove(entityManager.find(User.class, id));
+    }
+}
